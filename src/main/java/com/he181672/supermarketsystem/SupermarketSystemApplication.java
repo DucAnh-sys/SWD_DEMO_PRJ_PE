451b77f31ab2d19a7977f71cd9c235c0f55010ea
DucAnh-sys <anhbldhe181672@fpt.edu.vn>
2025-11-09 08:46:38 +0700

    propertise

---
+++ src/main/java/com/he181672/supermarketsystem/SupermarketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.he181672.supermarketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketSystemApplication.class, args);
+    }
+
+}
